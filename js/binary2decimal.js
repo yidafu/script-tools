22436bf103adda3c277f688fef5cf611f2520575
dafuyi <1427019390@qq.com>
2018-04-01 23:48:05 +0800

    add support to other number base

--- js/binary2decimal.js
+++ js/binary2decimal.js
@@ -1,22 +1,24 @@
 #!/usr/bin/env node
 
-let argv = process.argv.slice(2)
+let numberStr = process.argv.slice(2,3).toString()
 
-// switch ( argv[1] ) {
-//     case expression:
-//
-//         break;
-//     default:
-//
-// }
+let numberBase  = 0
+if( numberStr.startWith('0x') )
+    numberBase = 16
+else if ( numberBase.startWith('0') )
+    numberBase = 8
+else if ( numberStr.repeat('1') //  默认把全 1 0 的解析成二进制
+            +  numberStr.repeat('0')
+            === numberStr.length )
+    numberBase = 2
 
-let temp = 0
+let exponent = 0
 let res  = 0
 
 console.log(
     argv[0].split('').map(
         ( char ) => {
-            return parseInt( char ) * Math.pow( 8, temp ++ )
+            return parseInt( char ) * Math.pow( numberBase, exponent ++ )
         }
     ).map( val => res += val ).slice( -1 ).toString()
 )
