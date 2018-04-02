0020a240f83c674c8759fdf6902030d3d5d9dae3
dafuyi <1427019390@qq.com>
2018-04-02 17:27:39 +0800

    fix the order between multiplier and multiplicand

--- js/multiplication-table.js
+++ js/multiplication-table.js
@@ -4,7 +4,7 @@ console.log(
 
     [1,2,3,4,5,6,7,8,9].map( function( line, index, self ) {
         return self.slice(0, index + 1).map( function( col ) {
-             return line + "*" + col + "=" + ( line * col )
+             return col + "*" + line + "=" + ( line * col )
         } ).join( "\t" ).toString()
     } ).join( "\n" ).toString()
 
