f202cfb36a177962473159d3e045c68729f7dcf1
yidafu <yidafu@youzan.com>
2020-06-26 08:38:02 +0800

    feat: a script to release npm package

---
+++ npm-release.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Enter release version: " 
+
+read VERSION
+
+npm --git-tag-version version $VERSION -m "release: v$VERSION"
+
+npm publish --access public --registry https://registry.npmjs.com/
+
+git push origin v$VERSION
